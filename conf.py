ddd5e65100ba6ccf0a76ad6a39ec72503647ba8c
CharlesC <ccallawa@gmail.com>
2020-02-03 17:05:39 +0100

    Add Rinoh to conf.py but commented out

--- conf.py
+++ conf.py
@@ -37,8 +37,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.todo',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
 ]
+#     'rinoh.frontend.sphinx'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
